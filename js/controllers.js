25500da7425676eae67fed19e8dd28fea1f4e6b4
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-23 16:19:54 -0500

    added evCtrl

--- js/controllers.js
+++ js/controllers.js
@@ -3,4 +3,7 @@
 angular.module('myApp.controllers', []).
     controller('helloWorldCtrl', [function () {
         this.helloText = 'Hello from helloWorldCtrl';
+    } ]).
+    controller('evCtrl', [function () {
+        this.helloText = 'Hello from evCtrl';
     } ]);
