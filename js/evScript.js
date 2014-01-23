8d887350e8789471998c446a71f005ebb9bb9b5a
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-23 15:31:47 -0500

    Added zoom functionality

--- js/evScript.js
+++ js/evScript.js
@@ -4,3 +4,22 @@ for (var i = 0; i < project.layers.length; i++)
     project.layers[i].fitBounds(view.bounds);
     project.layers[i].scale(0.8);
 }
+function onKeyDown(event) {
+    if (event.key === 'w')
+    {
+        for (var i = 0; i < project.layers.length; i++)
+        {
+            project.layers[i].scale(0.8);
+            return false;
+        }
+    }
+    if (event.key === 's')
+    {
+        for (var i = 0; i < project.layers.length; i++)
+        {
+            project.layers[i].scale(0.8);
+            return false;
+        }
+    }
+    return true;
+}
