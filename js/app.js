57228645b0f90800db1e513f42231c4a0c1f4a46
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-23 15:17:27 -0500

    added route to ev view

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@ angular.module('myApp', [
     'ngRoute',
     'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/home', {templateUrl: 'partials/home.html', controller: 'helloWorldCtrl', controllerAs: 'hello'});
-    $routeProvider.otherwise({redirectTo: '/home'});
-    }]);
+config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/home', { templateUrl: 'partials/home.html', controller: 'helloWorldCtrl', controllerAs: 'hello' });
+    $routeProvider.when('/ev', { templateUrl: 'partials/EV.html', controller: 'evCtrl', controllerAs: 'ev' });
+    $routeProvider.otherwise({ redirectTo: '/home' });
+} ]);
