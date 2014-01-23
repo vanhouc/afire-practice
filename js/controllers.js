7500b6bda36ab74fb9e6d598fe0da5842e9cf9aa
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-23 17:32:31 -0500

    add scroll right on keypress

--- js/controllers.js
+++ js/controllers.js
@@ -4,7 +4,10 @@ angular.module('myApp.controllers', []).
     controller('helloWorldCtrl', [function () {
         this.helloText = 'Hello from helloWorldCtrl';
     } ]).
-    controller('evCtrl', [function () {
+    controller('evCtrl', [@scope, function ($scope) {
+        $scope.keyDown = function() {
+          view.scrollBy(new Point(50,0));  
+        };
         init();
         var path = new Path.Circle(new Point(80, 50), 30);
         path.strokeColor = 'black';
