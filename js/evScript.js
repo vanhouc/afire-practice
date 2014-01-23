8f38d0c53c7b9b36e9ee6f280b045e11c39b5ce1
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-23 15:50:22 -0500

    Created test function for evScript

--- js/evScript.js
+++ js/evScript.js
@@ -1,25 +1,7 @@
 var zf2 = project.importSVG(document.getElementById('svg'));
-for (var i = 0; i < project.layers.length; i++)
-{
-    project.layers[i].fitBounds(view.bounds);
-    project.layers[i].scale(0.8);
-}
-function onKeyDown(event) {
-    if (event.key === 'w')
-    {
-        for (var i = 0; i < project.layers.length; i++)
-        {
-            project.layers[i].scale(0.8);
-            return false;
-        }
-    }
-    if (event.key === 's')
-    {
-        for (var i = 0; i < project.layers.length; i++)
-        {
-            project.layers[i].scale(0.8);
-            return false;
-        }
-    }
-    return true;
-}
+var testPath = new Path.Circle(new Point(80, 50), 30);
+var testLayer = new Layer({
+    children: [path, path2],
+    strokeColor: 'black',
+    position: view.center
+});
