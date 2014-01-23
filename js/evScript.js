e46a5cd0d1f959ea50c26da78d7dd6ae4e2677be
Cameron VanHouzen <vanhouc@mail.nmc.edu>
2014-01-23 16:02:44 -0500

    accidentally invoking constructor

--- js/evScript.js
+++ js/evScript.js
@@ -1,6 +1,1 @@
-var testPath = new Path.Circle(new Point(80, 50), 30);
-var testLayer = new Layer({
-    children: [path, path2],
-    strokeColor: 'black',
-    position: view.center
-});
+var testPath = Path.Circle(new Point(80, 50), 30);
